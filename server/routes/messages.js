4fc0a1215c56d4582ee9a07351fa57209277a2b6
Varun Malaviya <varunmalaviya7@gmail.com>
2023-05-13 14:40:31 -0400

    added dotenv as dependency to store mongodb connection string as a secret variable. Added fucntion loadMessages() in messages.js to connect to mongodb database and load messages from project database.

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const mongodb = require('mongodb')
+require('dotenv').config()
 
 const router = express.Router()
 
-router.get('/', (req,res,next) => {
-    res.send('this is a message!')
+//GET requests
+router.get('/', async (req,res,next) => {
+    const messages = await loadMessages()
+    res.send(await messages.find({}).toArray())
 })
+
+//mongodb connection to connect to database
+async function loadMessages(){
+    const client = await mongodb.MongoClient.connect(process.env.DB_connection_string, {
+        useNewUrlParser: true
+    })
+
+    return client.db('message-board').collection('messages')
+}
 
 module.exports = router
