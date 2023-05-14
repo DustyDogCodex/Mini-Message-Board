0937ff9ac6a2baff347af0b8200263f85dfe0836
Varun Malaviya <varunmalaviya7@gmail.com>
2023-05-14 18:16:04 -0400

    trying to resolve 404 error on deployment. Fixing file paths

--- server/index.js
+++ server/index.js
@@ -16,17 +16,17 @@ app.use('/messages', messages)
 
 const port = process.env.PORT || 5000
 
-app.listen(port, () => [
-    console.log(`listening on port: ${port}`)
-])
-
 app.get('/*', (req,res) => {
     res.sendFile(
-        path.join(__dirname, "../client/build/index.html"),
+        path.join(__dirname, "/client/build/index.html"),
         function(err){
             if(err){
                 res.status(500).send(err)
             }
         }
     )
+})
+
+app.listen(port, () => {
+    console.log(`listening on port: ${port}`)
 })
